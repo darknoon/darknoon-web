42132eaa74530ba100f9925affdb47c0192c1610
Andrew Pouliot <andrew@darknoon.com>
2011-12-04 18:39:19 -0800

    Style & stuff

---
+++ style/darknoon.js
@@ -0,0 +1,15 @@
+//darknoon.js
+
+$(document).ready(function(){
+	
+	$(".highlight").each(function(){
+		var button = $('<a class="clipme">Copy to clipboard</a>');
+		$(button).zclip({
+			path: "/style/jquery/ZeroClipboard.swf",
+			copy: function() {
+				return $(this).next().text();
+			}
+		});
+		$(this).before(button);
+	});
+});
