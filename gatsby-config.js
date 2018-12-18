e6fed3bc42286a5d4dbc4c0ddc7edc877ac92e89
Andrew Pouliot <andrew@darknoon.com>
2018-12-18 11:20:17 -0500

    Set name

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'darknoon-gatsby',
+        short_name: 'darknoon',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
